562ff38b24771f02599f5c9da74b26b1ced3744c
NikolasDmn <diamantonikolas@gmail.com>
2022-11-08 16:12:55 +0100

    Comments and some error cases added. MB GUI?

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 use std::{char::from_u32, env, fs, io::Read};
 fn main() {
+    //executable needs argument of the file path of the .bf file
     let args: Vec<String> = env::args().collect();
-    let path = &args[1];
-    let text = get_file(&path);
-    interpret(&text.chars().collect());
+    if args.len() > 1 {
+        let path = &args[1];
+        let text = get_file(&path);
+        interpret(&text.chars().collect());
+    } else {
+        println!("File path was not provided");
+    }
 }
+// interpret takes as an argument a character vector of all characters in a .bf file
 fn interpret(c: &Vec<char>) {
+    //initialize the tape, loop_stack and the two pointers/indexes
     let mut tape = [0; 30000];
+    //loop_stack stores the index of the opening bracket for a loop
+    //so that it can be easily brought back at the closing bracket
     let mut loop_stack: Vec<usize> = vec![];
     let mut character_index = 0;
     let mut pointer = 0;
     while character_index < c.len() {
+        //match the character to its corresponding command
         match c[character_index] {
+            //store character_index-1 as we increment the index by one at the end of the while loop
             '[' => loop_stack.push(character_index - 1),
             ']' => {
                 if tape[pointer] != 0 {
@@ -29,7 +40,7 @@ fn interpret(c: &Vec<char>) {
                 let mut input: [u8; 1] = [0; 1];
                 std::io::stdin()
                     .read_exact(&mut input)
-                    .expect("failed to read stdin");
+                    .expect("Failed to read input");
                 tape[pointer] = input[0];
             }
             _ => {}
@@ -39,6 +50,6 @@ fn interpret(c: &Vec<char>) {
 }
 
 fn get_file(path: &str) -> String {
-    let text = fs::read_to_string(path).expect("Should have been able to read the file");
+    let text = fs::read_to_string(path).expect("Error reading file, check if it exists");
     return text;
 }
